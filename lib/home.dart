38315dc1ece7f4ed6b2d1ec09957ccb4d3af963a
Haresh Nayak <61956975+hareshnayak@users.noreply.github.com>
2021-11-22 18:14:42 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -99,8 +99,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
               ],
-            )),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            ),
+        ),
+      ), 
     );
   }
 }
